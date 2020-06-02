fef59371115f804e254cbd7be598e287a7ce4477
Tommy <t3ma2002@mail.ru>
2020-06-02 14:05:01 +0300

    adaptation slider

--- js/script.js
+++ js/script.js
@@ -6,16 +6,14 @@ $(document).ready(function () {
 		// adaptiveHeight: true,
 		autoplay: true,
 		autoplaySpeed: 2000,
-		prevArrow: '<button type="button" class="slick-prev"><img src="../img/left-solid.png"></button>',
-		nextArrow: '<button type="button" class="slick-next"><img src="../img/right-solid.png"></button>',
+		prevArrow: '<button type="button" class="slick-prev"><img src="img/left-solid.png"></button>',
+		nextArrow: '<button type="button" class="slick-next"><img src="img/right-solid.png"></button>',
 		responsive: [
 			{
-				breakpoint: 1024,
+				breakpoint: 992,
 				settings: {
-					slidesToShow: 3,
-					slidesToScroll: 3,
-					infinite: true,
-					dots: true
+					dots: true,
+					arrows: false
 				}
 			}
 		]
